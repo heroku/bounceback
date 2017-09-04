8a0faf3f2f3a544ac1e3fb3d32e37d00bd609167
Scott Clasen <scott@heroku.com>
2017-09-04 09:26:57 -0700

    wip

--- bounceback.go
+++ bounceback.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"os"
+	"bytes"
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
-func main()  {
+func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
+	go notify()
 
-
-	http.ListenAndServe(":" + port, handler())
+	http.ListenAndServe(":"+port, handler())
 }
 
-
 func handler() http.HandlerFunc {
-	return func(resp http.ResponseWriter, req *http.Request){
+	return func(resp http.ResponseWriter, req *http.Request) {
 		for k, _ := range pgbouncerUrls() {
 			resp.Write([]byte(k))
 		}
 	}
 }
-
 
 func pgbouncerUrls() map[string]string {
 	urls := map[string]string{}
@@ -35,7 +35,7 @@ func pgbouncerUrls() map[string]string {
 	for _, env := range envs {
 		log.Println(env)
 		kv := strings.Split(env, "=")
-		if strings.HasSuffix(kv[0], "_PGBOUNCER"){
+		if strings.HasSuffix(kv[0], "_PGBOUNCER") {
 			urls[kv[0]] = kv[1]
 		} else {
 			log.Println(kv[0])
@@ -48,9 +48,35 @@ func bouncebackUrls() []string {
 
 	urlz, set := os.LookupEnv("BOUNCEBACK_URLS")
 
-	if ! set {
+	if !set {
 		log.Println("fn=main at=no-bounceback-urls")
 	}
 
-	return strings.Split(urlz,",")
+	return strings.Split(urlz, ",")
+}
+
+func notify() {
+	pgUrls := pgbouncerUrls()
+	j, err := json.Marshal(pgUrls)
+	if err != nil {
+		log.Printf("func=notify at=json-err error=%q", err)
+		return
+	}
+
+	for _, url := range bouncebackUrls() {
+		req, err := http.NewRequest(url, "POST", bytes.NewReader(j))
+		if err != nil {
+			log.Printf("func=notify at=req-err error=%q", err)
+			continue
+		}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Printf("func=notify at=http-err error=%q", err)
+			continue
+		}
+
+		if resp.StatusCode > 299 {
+			log.Printf("func=notify at=http-code-err code=%d", resp.StatusCode)
+		}
+	}
 }
